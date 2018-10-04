f092fbd247b65a758dc32895acb197f1579c5b63
[zemenuyar] <[zemenuyar@gmail.com]>
2018-10-04 12:09:03 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //for loop
+        //syntax
+        //for(somevariable intialized; condition; somevariable = somevariable-2){
+        //instraction}
+        for (int x=0; x<10; x++) {
+            System.out.println("zemenu");
+        }
+        // infint for loop
+        for (int y=1; y>=1; y++) {
+            System.out.println("lion");
+
+
+
+        }
+    }
+}
